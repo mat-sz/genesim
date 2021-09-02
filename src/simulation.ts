ff244821ae562817aa635bc866e2fb2b3dd0fbdf
Mat Sz <contact@matsz.dev>
2021-09-02 18:03:02 +0200

    added an initial state function

--- src/simulation.ts
+++ src/simulation.ts
@@ -1,4 +1,5 @@
 export interface Settings {
+  startOrganismCount: number;
   mutationRate: number;
   inactiveBaseSurvivalRate: number;
   activeBaseSurvivalRate: number;
@@ -19,6 +20,9 @@ export enum Mutation {
 export interface Organism {
   id: number;
   mutation: Mutation;
+  x: number;
+  y: number;
+  age: number;
 }
 
 export interface State {
@@ -31,6 +35,29 @@ export function init(settings: Settings): State {
     currentId: 0,
     organisms: [],
   };
+
+  for (let i = 0; i < settings.startOrganismCount; i++) {
+    const firstAllelMutated = Math.random() < settings.mutationRate;
+    const secondAllelMutated = Math.random() < settings.mutationRate;
+
+    let mutation = Mutation.NONE;
+
+    if (firstAllelMutated && secondAllelMutated) {
+      mutation = Mutation.BOTH;
+    } else if (firstAllelMutated || secondAllelMutated) {
+      mutation = Mutation.ONE;
+    }
+
+    const organism: Organism = {
+      id: state.currentId++,
+      age: 0,
+      mutation,
+      x: Math.random(),
+      y: Math.random(),
+    };
+
+    state.organisms.push(organism);
+  }
 
   return state;
 }
